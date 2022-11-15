3cd7ac6eb7d0dc1cbae82478ae0beefdd4898b99
RalfKonrad <github@eckel-consulting.de>
2022-11-15 12:17:34 +0100

    Skeleton swaption classes

---
+++ ql/experimental/hullwhitewithtwocurves/pricingengines/swaption/hw2ctreeswaptionengine.hpp
@@ -0,0 +1,31 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*
+ Copyright (C) 2022 Ralf Konrad Eckel
+
+ This file is part of QuantLib, a free-software/open-source library
+ for financial quantitative analysts and developers - http://quantlib.org/
+
+ QuantLib is free software: you can redistribute it and/or modify it
+ under the terms of the QuantLib license.  You should have received a
+ copy of the license along with this program; if not, please email
+ <quantlib-dev@lists.sf.net>. The license is also available online at
+ <http://quantlib.org/license.shtml>.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ FOR A PARTICULAR PURPOSE.  See the license for more details.
+*/
+
+/*! \file hw2ctreeswaptionengine.hpp
+    \brief
+*/
+
+#ifndef quantlib_hw2c_tree_swaption_engine_hpp
+#define quantlib_hw2c_tree_swaption_engine_hpp
+
+namespace QuantLib {
+    class HW2CTreeSwaptionEngine {};
+}
+
+#endif // quantlib_hw2c_tree_swaption_engine_hpp
